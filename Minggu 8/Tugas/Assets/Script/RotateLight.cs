1cf27ae80345ddf022de69cfabff150833c29422
raihanrach <raihanrach@gmail.com>
2021-04-12 03:55:54 +0700

    Minggu 8

---
+++ Minggu 8/Tugas/Assets/Script/RotateLight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    public float speed = -1.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+
+}
